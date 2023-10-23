86e89c193284f3a3b4964485cf9eab827b42151b
oliverkangu <78103906+oliverkangu@users.noreply.github.com>
2023-10-23 18:20:30 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,124 @@
+const items = [
+    { id: 1, name: "apple", price: 1.75, categoryId: 1, inventory: 100 },
+    { id: 2, name: "banana", price: 0.25, categoryId: 1, inventory: 100 },
+    { id: 3, name: "orange", price: 1.0, categoryId: 1, inventory: 100 },
+    { id: 4, name: "broccoli", price: 3.0, categoryId: 2, inventory: 100 },
+    { id: 5, name: "cucumber", price: 1.0, categoryId: 2, inventory: 100 },
+    { id: 6, name: "milk", price: 5.75, categoryId: 3, inventory: 100 },
+    { id: 7, name: "cheddar cheese", price: 4.0, categoryId: 3, inventory: 100 },
+    { id: 8, name: "sourdough loaf", price: 5.5, categoryId: 4, inventory: 100 },
+  ];
+  
+  const cart = [];
+  
+  function logItemNames() {
+    items.forEach((currentItem) => console.log(currentItem.name));
+  }
+  
+  function findItemById(id) {
+    return items.find((currentItem) => currentItem.id === id);
+  }
+  
+  function capitalizeNames() {
+    return items.map((currentItem) => {
+      const name = currentItem.name;
+      const newName = name.slice(0, 1).toUpperCase() + name.slice(1);
+      currentItem.name = newName;
+      return currentItem;
+    });
+  }
+  
+  function calculateTotalInventory() {
+    return items.reduce((totalInventory, currentItem) => {
+      return totalInventory + currentItem.inventory;
+    }, 0);
+  }
+  
+  function calculateAllInventoryPrice() {
+    return items.reduce((totalInventoryPrice, currentItem) => {
+      const totalItemPrice = currentItem.price * currentItem.inventory;
+      return totalInventoryPrice + totalItemPrice;
+    }, 0);
+  }
+  
+  function getItemPriceByName(name) {
+    const item = items.find(
+      (currentItem) => currentItem.name.toLowerCase() === name.toLowerCase()
+    );
+    return item.price;
+  }
+  
+  function filterItemsByCategoryId(categoryId) {
+    return items.filter((currentItem) => currentItem.categoryId === categoryId);
+  }
+  
+  function logCartItems() {
+    for (let i = 0; i < cart.length; i++) {
+      const currentId = parseInt(cart[i]);
+      const item = findItemById(currentId);
+      console.log(item.name);
+    }
+  }
+  
+  function calculateTotalCartPrice() {
+    let totalPrice = 0;
+    cart.forEach((currentItemId) => {
+      const item = findItemById(parseInt(currentItemId));
+      totalPrice += item.price;
+    });
+    return totalPrice;
+  }
+  
+  const ids = prompt(
+    "enter numbers separated by commas for the ids of the items you want to add to your cart",
+    "1, 3, 5"
+  );
+  const idArr = ids.split(", ");
+  
+  idArr.forEach((id) => cart.push(id));
+  console.log(`The names of all the items are: `);
+  logItemNames();
+  const itemId = prompt("enter the id of an item you are trying to find", "1");
+  console.log(
+    `The item with id ${itemId} is  ${JSON.stringify(
+      findItemById(+itemId),
+      null,
+      2
+    )}`
+  );
+  console.log(
+    "We can map over an array and return a new array with the names capitalized like so: ",
+    capitalizeNames()
+  );
+  console.log(
+    "The total inventory of all grocery items is: ",
+    calculateTotalInventory()
+  );
+  console.log(
+    "The total price of all items in inventory is: ",
+    calculateAllInventoryPrice()
+  );
+  
+  const itemToFind = prompt(
+    "Enter the name of an item to find the price of",
+    "apple"
+  );
+  console.log(`The price of ${itemToFind} is: `, getItemPriceByName(itemToFind));
+  
+  const categoryId = prompt(
+    "Enter a number between 1-4 to filter only items with that categoryId",
+    2
+  );
+  console.log(
+    `The items in category ${categoryId} are: `,
+    filterItemsByCategoryId(+categoryId)
+  );
+  
+  console.log("Cart items: ");
+  logCartItems();
+  
+  console.log(
+    `The total price of the items in your cart is: `,
+    calculateTotalCartPrice()
+  );
+  
